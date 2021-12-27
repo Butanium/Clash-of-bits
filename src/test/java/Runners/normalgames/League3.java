56e251223263c98bc9053ee24cca3e6f1a978eec
Clement Dumas <dumasclement2002@gmail.com>
2021-12-27 12:37:49 +0100

    removed the set seed thing

--- src/test/java/Runners/normalgames/League3.java
+++ src/test/java/Runners/normalgames/League3.java
@@ -21,7 +21,7 @@ public class League3 {
 
       //  gameRunner.addAgent(outsider1.class);
         gameRunner.setLeagueLevel(3);
-        gameRunner.setSeed(-8380311610760221388L);
+//        gameRunner.setSeed(-8380311610760221388L);
         gameRunner.start();
         // Another way to add a player
         //gameRunner.addAgent("python3 C:/Users/Clement/Documents/coding/codinGame/game-skeleton/src/test/java/basicStub.py3");
