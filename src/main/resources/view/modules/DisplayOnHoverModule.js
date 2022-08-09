59985ce5ac13fcffd9782949b0d8e578bc714461
Clement Dumas <dumasclement2002@gmail.com>
2022-08-09 18:49:30 +0200

    updated the display on hover module

--- src/main/resources/view/modules/DisplayOnHoverModule.js
+++ src/main/resources/view/modules/DisplayOnHoverModule.js
@@ -1,26 +1,9 @@
 // noinspection JSUnusedGlobalSymbols
 
 import {api as entityModule} from '../entity-module/GraphicEntityModule.js'
+import {WIDTH, HEIGHT} from '../core/constants.js'
 
 /* global PIXI */
-function getMouseOverFunc(id, module) {
-    return function () {
-        module.inside[id] = true
-    }
-}
-
-function getMouseOutFunc(id, module) {
-    return function () {
-        delete module.inside[id]
-        if (module.currently_debugged.has(id)) {
-            module.currently_debugged.delete(id)
-            for (let debug_id of module.currentFrame.registered[id]) {
-                const debug_entity = entityModule.entities.get(debug_id)
-                debug_entity.container.visible = false
-            }
-        }
-    }
-}
 
 function getEntityCurrentSubStates(entity, frame) {
     if (entity.states[frame]) {
@@ -41,6 +24,77 @@ function getEntityState(entity, frame) {
         return subStates[subStates.length - 1]
     }
     return null
+}
+
+function hideAssociated(id, module) {
+    for (let debug_id of module.currentFrame.registered[id]) {
+        const debug_entity = entityModule.entities.get(debug_id)
+        debug_entity.container.visible = false
+    }
+}
+
+function showAssociated(id, module) {
+    for (let debug_id of module.currentFrame.registered[id]) {
+        const debug_entity = entityModule.entities.get(debug_id)
+        debug_entity.container.visible = true
+    }
+}
+
+function showAllAssociated(ids, module) {
+    for (let id of ids) {
+        showAssociated(id, module)
+    }
+}
+
+function getMouseOverFunc(id, module) {
+    return function () {
+        module.inside[id] = true
+    }
+}
+
+function getMouseOutFunc(id, module) {
+    return function () {
+        delete module.inside[id]
+        if (module.currently_debugged.has(id)) {
+            module.currently_debugged.delete(id)
+            if (!module.permanent_debug.has(id)) {
+                hideAssociated(id, module)
+            }
+        }
+    }
+}
+
+function getMouseClickFunc(id, module) {
+    return function (ev) {
+        const mouseEvent = ev.data.originalEvent
+        if (mouseEvent instanceof MouseEvent) {
+            if (mouseEvent.button === 0) {
+                if (module.permanent_debug.has(id)) {
+                    module.permanent_debug.delete(id)
+                    hideAssociated(id, module)
+                } else if (!mouseEvent.altKey) {
+                    module.permanent_debug.add(id)
+                    showAssociated(id, module)
+                }
+            }
+        }
+    }
+
+}
+
+function getResetOnClickFunc(module) {
+    return function (ev) {
+        console.log("reset done")
+        const mouseEvent = ev.data.originalEvent
+        if (mouseEvent instanceof MouseEvent) {
+            if (mouseEvent.altKey) {
+                for (let permanent_id of module.permanent_debug) {
+                    hideAssociated(permanent_id, module)
+                }
+                module.permanent_debug = new Set()
+            }
+        }
+    }
 }
 
 function getMouseMoveFunc(module) {
@@ -66,10 +120,7 @@ function getMouseMoveFunc(module) {
                     const state = getEntityState(entity, module.currentFrame.number)
                     if (state !== null && module.currentFrame.registered[show] !== null) {
                         module.currently_debugged.add(show)
-                        for (let debug_id of module.currentFrame.registered[show]) {
-                            const debug_entity = entityModule.entities.get(debug_id)
-                            debug_entity.container.visible = true
-                        }
+                        showAssociated(show, module)
                     }
                 }
             } else {
@@ -77,7 +128,7 @@ function getMouseMoveFunc(module) {
                 const to_remove = []
                 for (let id of module.currently_debugged) {
                     const register = module.currentFrame.registered[id]
-                    if (register.length === 0){
+                    if (register.length === 0) {
                         to_remove.push(id)
                         delete module.inside[id]
                     }
@@ -88,14 +139,11 @@ function getMouseMoveFunc(module) {
                 if (module.currently_debugged.size === 0) {
                     const available = showing.filter(id =>
                         getEntityState(entityModule.entities.get(id), module.currentFrame.number) !== null
-                            && module.currentFrame.registered[id] !== null)
+                        && module.currentFrame.registered[id] !== null)
                     if (available.length) {
                         const min = Math.min(...available)
                         module.currently_debugged.add(min)
-                        for (let debug_id of module.currentFrame.registered[min]) {
-                            const debug_entity = entityModule.entities.get(debug_id)
-                            debug_entity.container.visible = true
-                        }
+                        showAssociated(min, module)
                     }
                 }
             }
@@ -104,13 +152,14 @@ function getMouseMoveFunc(module) {
 }
 
 export class DisplayOnHoverModule {
-    constructor(assets) {
+    constructor(_assets) {
         this.interactive = {}
         this.previousFrame = {
             registered: {}
         }
         this.inside = {}
         this.currently_debugged = new Set()
+        this.permanent_debug = new Set()
         this.lastProgress = 1
         this.lastFrame = 0
     }
@@ -122,12 +171,8 @@ export class DisplayOnHoverModule {
     updateScene(previousData, currentData, progress) {
         this.currentFrame = currentData
         this.currentProgress = progress
-        for (let id of this.currently_debugged) {
-            for (let debug_id of this.currentFrame.registered[id]) {
-                const debug_entity = entityModule.entities.get(debug_id)
-                debug_entity.container.visible = true
-            }
-        }
+        showAllAssociated(this.currently_debugged, this)
+        showAllAssociated(this.permanent_debug, this)
     }
 
     handleFrameData(frameInfo, data = []) {
@@ -148,16 +193,24 @@ export class DisplayOnHoverModule {
     reinitScene(container) {
         entityModule.entities.forEach(entity => {
             if (this.interactive[entity.id]) {
+                // container is the pixi object which is displayed to represent the entity
                 entity.container.interactive = true
                 entity.container.mouseover = getMouseOverFunc(entity.id, this)
                 entity.container.mouseout = getMouseOutFunc(entity.id, this)
+                entity.container.click = getMouseClickFunc(entity.id, this)
             }
         })
         this.container = container
         container.interactive = true
         container.mousemove = getMouseMoveFunc(this)
+        const entityContainer = entityModule.container
+        entityContainer.interactive = true
+        entityContainer.click = getResetOnClickFunc(this)
     }
+
 
 }
 
 DisplayOnHoverModule.allow_multiple_debug = false
+// todo implement below
+DisplayOnHoverModule.allow_multiple_static = true
