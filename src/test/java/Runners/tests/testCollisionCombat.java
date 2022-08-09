9fb46972c0827f3b4094f130546af0ad7e8c4a05
Clement Dumas <dumasclement2002@gmail.com>
2022-08-09 18:50:23 +0200

    added collision test

---
+++ src/test/java/Runners/tests/testCollisionCombat.java
@@ -0,0 +1,30 @@
+package Runners.tests;
+
+import agents.boss2;
+import agents.boss3;
+import com.codingame.gameengine.runner.MultiplayerGameRunner;
+
+public class testCollisionCombat {
+    public static void main(String[] args) {
+        /* Multiplayer Game */
+        MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
+
+        // Adds as many player as you need to test your game
+        //gameRunner.addAgent(Agent1.class);
+        gameRunner.addAgent(boss3.class);
+        gameRunner.addAgent(boss2.class);
+
+
+        //gameRunner.addAgent("js C:/Users/Clement/Documents/coding/codinGame/game-skeleton/src/test/java/testMaxime.js");
+
+        //gameRunner.addAgent(league2outsider.class);
+
+      //  gameRunner.addAgent(outsider1.class);
+        gameRunner.setLeagueLevel(3);
+        gameRunner.setSeed(9201176636923838100L);
+        gameRunner.start();
+        // Another way to add a player
+        //gameRunner.addAgent("python3 C:/Users/Clement/Documents/coding/codinGame/game-skeleton/src/test/java/basicStub.py3");
+
+    }
+}
