182fef1da697825dc0d1f2e694d91d3aeef6a591
Clement Dumas <dumasclement2002@gmail.com>
2022-08-22 01:20:25 +0200

    minor reformat

--- src/main/java/com/codingame/game/action/Attack.java
+++ src/main/java/com/codingame/game/action/Attack.java
@@ -2,15 +2,16 @@ package com.codingame.game.action;
 
 import com.codingame.game.gameEntities.Robot;
 
-public class Attack extends Action{
-    public Attack(Robot executor, Robot target){
-        super(executor,1);
+public class Attack extends Action {
+    public Attack(Robot executor, Robot target) {
+        super(executor, 1);
         this.target = target;
     }
+
     private final Robot target;
 
     @Override
-    public void performAction(){
+    public void performAction() {
         getExecutor().ATTACK(target);
     }
 }
